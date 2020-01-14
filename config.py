f35f44c6cf02119ee161d296d20da20086b5fcdd
Sara Rey <sara.rey.manchester@gmail.com>
2020-01-14 13:55:24 +0000

    worksheet id identified

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 import os
 
 # Paths
+'''
+archive_directory_cluster = os.path.join(os.sep, "A:", os.sep, "nextseq")
+results_directory_cluster = os.path.join(os.sep, "W:", os.sep)
+results_directory_l_drive = os.path.join(os.sep, "L:", os.sep, "NGS ANALYSIS", "TruSightCancer")
+'''
 
-results_directory = os.path.join()
+archive_directory_cluster = os.path.join(os.sep, "Users", "sararey", "Documents", "tsca", "raw")
+results_directory_cluster = os.path.join(os.sep, "Users", "sararey", "Documents", "tsca")
+results_directory_l_drive = os.path.join(os.sep, "Users", "sararey", "Documents", "tsca", "results")
--- copy_tsca.py
+++ copy_tsca.py
@@ -1,11 +1,55 @@
+import os
+from datetime import datetime
+
+# Global variables
+from config import archive_directory_cluster
+from config import results_directory_cluster
+from config import results_directory_l_drive
+
+# Temp variables
+run_id = "191220_NB551415_0052_AHVVJVAFXY"
 
 
+def parse_variables():
+    variables = {}
+    # Load variables files for all samples on the run
+    for s in next(os.walk(os.path.join(results_directory_cluster, run_id, "IlluminaTruSightCancer")))[1]:
+        current_variables = {}
+        with open(os.path.join(results_directory_cluster, run_id, "IlluminaTruSightCancer", s,
+                               f"{s}.variables")) as vf:
+            for line in vf:
+                split_line = line.rstrip().split("=")
+                try:
+                    current_variables[split_line[0]] = split_line[1]
+                except IndexError:
+                    pass
+        variables[s] = current_variables
+    return variables
 
 
 def main():
-    # Load config file
-    conf = ""
+    # Obtain current year
+    yr = datetime.now().year
+
+    # Check existence of results directory on L: and if not there, create it
+    if not os.path.exists(os.path.join(results_directory_l_drive, f"{yr} Runs")):
+        os.makedirs(os.path.join(results_directory_l_drive, f"{yr} Runs"))
+
     # Obtain worksheet id
+    # Load variables files
+    all_variables = parse_variables()
+
+    # Extract worksheet id from variables files
+    worksheets = []
+    for s, d in all_variables.items():
+        worksheets.append(d.get('worklistId'))
+    worksheet_ids = list(set(worksheets))
+
+    if len(worksheet_ids) != 1:
+        raise Exception(f"Unable to identify the worksheet id for run {run_id}. Program Exiting.")
+
+    worksheet_id = worksheet_ids[0]
+    print(worksheet_id)
 
 
 if __name__ == '__main__':
